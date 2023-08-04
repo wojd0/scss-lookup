a6df2cf4c9503170a3e8e1d0fc06a5316257b8d5
Wojciech Duda <69160975+wojd0@users.noreply.github.com>
2023-08-04 11:59:57 +0200

    final version, I guess

--- lookup.py
+++ lookup.py
@@ -1,42 +1,42 @@
 import os
 import re
 from pathlib import Path
+from dotenv import load_dotenv
+from colorama import Fore
+load_dotenv()
 
 root_dir = os.environ['ROOT_DIR']
 
-def extract_first_word(string):
-    pattern = r'[a-zA-Z-]'
-    match = ''.join(re.findall(pattern, string))
-    return match
+pattern = r'--[a-z-]+'
 
 
 def find_string_in_directory(root_dir, search_strings):
     occurrences = {}
+    for search_string in search_strings:
+        occurrences[search_string] = []
+
     os.chdir(root_dir)
     for file_path in Path(root_dir).rglob(f"*.{os.environ['EXTENSION']}"):
-        with open(file_path, 'r') as file_path:
-            for line_number, line in enumerate(file_path, 1):
-                  for search_string in search_strings:
-                     if search_string in line:
-                           occurrences[search_string] = occurrences.get(search_string, 0) + 1
+        with open(file_path, 'r') as file:
+            for line_number, line in enumerate(file, 1):
+                for search_string in search_strings:
+                    if search_string in line:
+                        occurrences[search_string].append(file.name)
     return occurrences
 
 
+with open('./varlist.txt', 'r') as lookup_file:
+    ready_lines = [re.findall(pattern, line)[0] for line in lookup_file if re.findall(pattern, line)]
 
-with open('/Users/wojciech.duda/Dev/tolookfor.txt', 'r') as lookup_file:
-    ready_lines = []
-    for line in lookup_file:
-        line = line.strip()
-
-        if  '--' not in line: continue
-
-        last_index = line.index(')') if ')' in line else line.index(':')
-        line = line[line.index('--'): last_index]
-        ready_lines += [line]
-        # line = extract_first_word(line)
     result = find_string_in_directory(root_dir, ready_lines)
-    print('Tested ', len(result), ' variables')
+
+    print(Fore.GREEN, 'Tested ', len(result), ' variables')
+    print(Fore.BLUE, 'Printing variables that did not occur', Fore.RED, os.environ['SKIP_AMOUNT'], Fore.BLUE, 'times in the entire project')
+
     for r in result:
-         if result[r] != os.environ['SKIP_AMOUNT']:
-             print(r,' <=> ', result[r])
+        if len(result[r]) != int(os.environ['SKIP_AMOUNT']):
+            print()
+            print(Fore.RED, r, ' => ', len(result[r]))
+            for path in result[r]:
+                print(Fore.RESET, '    ', path)
     # print(line)
