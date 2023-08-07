834bc0b87def3f94a81a5228fab03329a1127c1d
Wojciech Duda <69160975+wojd0@users.noreply.github.com>
2023-08-07 10:48:58 +0200

    update readme and vars

--- test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-string = '--studio-process-category-selector-input-label-error-color: $studio-text-input-label-error-color,--studio-process-category-selector-input-label-error-color: $studio-text-input-label-error-color,'
-expression = r'--[a-z-]+'
-
-print(re.findall(expression, string))
-print(re.match(expression, ''))
